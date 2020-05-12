35e6e80a0ffb9bee7f7b6d58ae99aa98d633b57e
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-05-12 09:47:41 +0200

    refactor: use the repeat function to avoid double declaration

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,5 +3,5 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: grid;
   height: 100%;
-  grid-template-rows: min-content min-content 1fr;
+  grid-template-rows: repeat(2, min-content) 1fr;
 `;
