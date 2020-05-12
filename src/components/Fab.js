ff0dad090c2e6de6846e7691475f10bcf37945a5
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-05-12 09:29:45 +0200

    refactor: export a base container to collect various Fabs

--- src/components/Fab.js
+++ src/components/Fab.js
@@ -1,27 +1,39 @@
 import React from "react";
 import { createPortal } from "react-dom";
 import styled from "styled-components";
+import { space } from "@styled-system/space";
 
-const Base = styled.button`
-  position: absolute;
-  bottom: 32px;
-  right: 32px;
+export const Fab = styled.button`
+  ${space({ mb: 2 })};
   border-radius: 50%;
   height: 48px;
   width: 48px;
   font-size: 24px;
   background: ${({ theme }) => theme.primary};
-  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.4);
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4);
 
-  z-index: 200;
+  cursor: ${({ onClick }) => (!!onClick ? "pointer" : "default")};
+
+  &:hover {
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.4);
+  }
 `;
 
-export const Fab = (props) => {
-  const root = document.getElementById("root");
+const Base = styled.div`
+  position: fixed;
+  z-index: 200;
+  bottom: 16px;
+  right: 16px;
+  display: flex;
+  flex-direction: column-reverse;
+`;
+
+export const FabBase = ({ portal = null, children }) => {
+  const root = portal ?? document.body;
 
   if (!root) {
     return null;
   }
 
-  return createPortal(<Base {...props} />, root);
+  return createPortal(<Base>{children}</Base>, root);
 };
