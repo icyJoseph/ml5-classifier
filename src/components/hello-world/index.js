ffd4df72ea0c4630875feeb18e8094a3d8fd8fc0
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-05-20 13:28:58 +0200

    feat: classify clicked images - no control, no UI reflection

--- src/components/hello-world/index.js
+++ src/components/hello-world/index.js
@@ -1,11 +1,35 @@
 import { h, Component } from "preact";
-import "./style.scss";
+import ml5 from "ml5";
+import widget from "./style.scss";
 
 export default class App extends Component {
-  render(props) {
+  state = {
+    modelLoaded: false
+  };
+
+  classifier = null;
+
+  componentDidMount() {
+    this.classifier = ml5.imageClassifier("Mobilenet", () =>
+      this.setState({ modelLoaded: true })
+    );
+    window.addEventListener("click", this.classify.bind(this));
+  }
+
+  classify(e) {
+    return this.classifier.classify(e.target, (err, results) => {
+      if (err) return console.log(err);
+      console.log(results);
+    });
+  }
+
+  render(props, state) {
+    const { modelLoaded } = state;
+    const { color } = props;
     return (
-      <div>
-        <h1 style={{ color: props.color }}>Hello, World!</h1>
+      <div className={widget.widget}>
+        <h1 style={{ color }}>Hello, World!</h1>
+        <p>Model {modelLoaded ? "loaded" : "not-loaded"}</p>
       </div>
     );
   }
